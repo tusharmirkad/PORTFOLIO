d0617939156835fd86c47cc29a40315c7724c778
tusharmirkad <tusharmirkad@gmail.com>
2025-02-10 17:11:23 +0530

    Update Resume

--- my-app/app/components/Header.jsx
+++ my-app/app/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
             <motion.a
              initial={{y: 30, opacity:0}}
              whileInView={{y: 0, opacity: 1}}
-             transition={{duration:0.6, delay: 1.2}} href="/tushar_mirkad.pdf" download className='px-10 py-3 border rounded-full
+             transition={{duration:0.6, delay: 1.2}} href="/Tushar_Mirkad_Resume.pdf" download className='px-10 py-3 border rounded-full
              border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='rounded-full w-4'/></motion.a>
         </div>
     </div>
